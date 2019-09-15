7ba8a8d5201ab42ab32a74c2b996217eaaf7c8a8
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-15 14:22:04 -0400

    ci updates new JSON structures

--- ci/scripts/update-buildpack.sh
+++ ci/scripts/update-buildpack.sh
@@ -12,7 +12,8 @@ filename=$(basename $(ls $GITHUB_REPO/*.zip))
 git clone git pushme
 cd pushme
 
-sed -i "s%.*\b$BUILDPACK_NAME\b.*$%cf update-buildpack -s cflinuxfs3 --enable $BUILDPACK_NAME -p https://github.com/$GITHUB_ORG/$GITHUB_REPO/releases/download/v${version}/${filename}%" update-only.sh
+sed -i "s%.*\"$BUILDPACK_NAME\".*$%  \"$BUILDPACK_NAME\": \"https://github.com/$GITHUB_ORG/$GITHUB_REPO/releases/download/v${version}/${filename}\",%" buildpacks.json
+sed -i "s%.*\"$BUILDPACK_NAME\".*$%  \"$BUILDPACK_NAME\": \"https://github.com/$GITHUB_ORG/$GITHUB_REPO/releases/download/v${version}/${filename}\",%" update-only.sh
 
 if [[ "$(git status -s)X" != "X" ]]; then
   if [[ -z $(git config --global user.email) ]]; then
@@ -21,6 +22,7 @@ if [[ "$(git status -s)X" != "X" ]]; then
   if [[ -z $(git config --global user.name) ]]; then
     git config --global user.name "CI Bot"
   fi
+  git add buildpacks.json
   git add update-only.sh
   git commit -m "Updated $BUILDPACK_NAME to v${version}"
 fi
