9655ad50bcbdc392baafd25932efd71585f2458a
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-15 02:33:28 -0400

    add ruby

--- update-only.sh
+++ update-only.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cf update-buildpack -s cflinuxfs3 --enable go_buildpack -p https://github.com/cloudfoundry/go-buildpack/releases/download/v1.9.0/go-buildpack-cflinuxfs3-v1.9.0.zip
+cf update-buildpack -s cflinuxfs3 --enable ruby_buildpack -p xxx
