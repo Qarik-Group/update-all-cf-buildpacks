623a5e69a9a7dc1b329e410cd90495335c959e11
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-15 02:45:08 -0400

    add java

--- update-only.sh
+++ update-only.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cf update-buildpack -s cflinuxfs3 --enable go_buildpack -p https://github.com/cloudfoundry/go-buildpack/releases/download/v1.9.0/go-buildpack-cflinuxfs3-v1.9.0.zip
-cf update-buildpack -s cflinuxfs3 --enable ruby_buildpack -p https://github.com/cloudfoundry/ruby-buildpack/releases/download/v1.7.43/ruby-buildpack-cflinuxfs3-v1.7.43.zip
+cf update-buildpack -s cflinuxfs3 --enable ruby_buildpack -p https://github.com/cloudfoundry/java-buildpack/releases/download/v4.21/java-buildpack-v4.21.zip
+cf update-buildpack -s cflinuxfs3 --enable java_buildpack 
